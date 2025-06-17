6f6b34ff63db094e2e68a0fd7287ac4fcd5032f5
prernaamakwana <prernaamakwana07@gmail.com>
2025-06-17 10:01:33 +0530

    Connect to Azure Ai Foundary

--- ai-foundary.js
+++ ai-foundary.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
  
 dotenv.config();
 const client = new ModelClient(
-    process.env.AZURE_INFERENCE_SDK_ENDPOINT ?? "https://prern-mbyjp52y-eastus2.services.ai.azure.com/models", new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "8alfuE0f4DgFl7ZUkS1QtkEQCkFzvVyU7uKbyYAKuaTBvzrqCnUOJQQJ99BFACHYHv6XJ3w3AAAAACOGBRA5"));
+    process.env.AZURE_INFERENCE_SDK_ENDPOINT ?? "https://prern-mc00j4f8-swedencentral.services.ai.azure.com/models", new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "8eFeEMG5z4ipkuD2WmCCvNlgdB8aGTFzynkSdsNirH8bevZsvZHRJQQJ99BFACfhMk5XJ3w3AAAAACOGUvNn"));
  
 var messages = [
     { role: "developer", content: "You are an helpful assistant" },
-    { role: "user", content: "What are 3 things to see in INDIA?" },
+    { role: "user", content: "What are 3 things to see in UDAIPUR?" },
 ];
  
 var response = await client.path("chat/completions").post({
